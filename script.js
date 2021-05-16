103e762c02c1c5345f3984843ef8a5a68577953e
duygugurbuzyildiz <gurbuz.duygu@gmail.com>
2021-05-16 20:11:18 +0200

    Arranged html and css files. Added daily and hourly forecasts to js files.

--- script.js
+++ script.js
@@ -21,6 +21,109 @@ function formatDate (){
     return `${currentDay}, ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp){
+    let date = new Date(timestamp * 1000);
+    let day = date.getDay();
+
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+    return days[day];
+}
+
+function formatHour(timestamp){
+    let time = new Date(timestamp * 1000);
+    let hours = time.getHours();
+
+    if (hours < 10){
+
+    return `0${hours}:00`;
+    }
+    else {
+        return `${hours}:00`
+    }
+}
+
+function formatSunRiseAndSet(timestamp){
+    let time = new Date (timestamp * 1000);
+    let hours = time.getHours();
+    let minutes = time.getMinutes();
+    
+    if (minutes < 10){
+        minutes = `0${minutes}`;
+    }
+    if (hours < 10){
+    hours = `0${hours}`;
+    }
+
+    return `${hours}:${minutes}`;
+
+}
+
+function displayForecast(response) {
+    console.log(response.data);
+    let forecast = response.data.daily;
+
+    let forecastElement = document.querySelector("#forecast");
+
+    let forecastHTML = `<div class="row g-3 mt-2" style="text-align: center;">`;
+
+    forecast.forEach(function (forecastDay, index){
+        if (index < 6){
+
+            let tempMax = Math.round(forecastDay.temp.max);
+            let tempMin = Math.round (forecastDay.temp.min);
+            
+            forecastHTML = forecastHTML +
+            `<div class="col-2">
+                <div class="forecastDate">
+                ${formatDay(forecastDay.dt)}
+                </div>
+                <img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" alt="" width="42"/>
+                <div class="forecastTemperature">
+                    <span class="weather-forecast-temperature-max">${tempMax}</span>ºC   
+                    <span class="weather-forecast-temperature-min">${tempMin}</span>ºC
+                </div>
+            </div>
+            `;
+
+        }
+
+    });
+    forecastHTML = forecastHTML +`</div>`;
+    forecastElement.innerHTML = forecastHTML;
+
+    let hourlyElement = response.data.hourly;
+
+    let forecastHourlyElement = document.querySelector("#forecast-hourly");
+
+    let forecastHourlyHTML = `<div class="row">`;
+
+    hourlyElement.forEach(function(forecastHour, index){
+        if (index < 12){
+            
+            forecastHourlyHTML = forecastHourlyHTML + 
+            `
+                <div class="col-1" style="text-align: center;" id="hourly-temperature">
+                    <div class="temperature-value-hourly" id="temperature-hourly">${Math.round(forecastHour.temp)}ºC</div>
+                        <img src="http://openweathermap.org/img/wn/${forecastHour.weather[0].icon}@2x.png" alt="" width="35"/>
+                        <div class="hourlyTime" id="hourly-time">${formatHour(forecastHour.dt)}</div>
+                </div>
+            `;
+        }
+    });
+    
+    forecastHourlyHTML = forecastHourlyHTML + `</div>`;
+    forecastHourlyElement.innerHTML = forecastHourlyHTML;
+
+}
+
+function getForecast (coordinates){
+    let apiKey = "ed0b705eed2cc81cd6b87bb194b4d152";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayForecast);
+
+}
+
 function displayTemperature (response){
     
     let temperatureElement = document.querySelector("#city-temperature-value");
@@ -31,11 +134,13 @@ function displayTemperature (response){
     let pressureElement = document.querySelector("#pressure");
     let feelsLikeElement = document.querySelector("#feels-like");
     let dateElement = document.querySelector("#current-date");
+    let iconElement = document.querySelector("#main-city-icon");
+    let sunRiseElement = document.querySelector("#sun-rise");
+    let sunSetElement = document.querySelector("#sun-set");
 
     celsiusTemperature = response.data.main.temp;
 
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
-    console.log(response.data);
     cityElement.innerHTML = response.data.name;
     descriptionElement.innerHTML = response.data.weather[0].description;
     humidityElement.innerHTML = response.data.main.humidity;
@@ -43,6 +148,11 @@ function displayTemperature (response){
     pressureElement.innerHTML = response.data.main.pressure;
     feelsLikeElement.innerHTML = Math.round(response.data.main.feels_like);
     dateElement.innerHTML = formatDate();
+    iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+    sunRiseElement.innerHTML = formatSunRiseAndSet(response.data.sys.sunrise);
+    sunSetElement.innerHTML = formatSunRiseAndSet(response.data.sys.sunset);
+    
+    getForecast (response.data.coord);
 }
 
 function search (city){
