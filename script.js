8a22269fc7899855c3d92abbf0ad2a6a446afb02
duygugurbuzyildiz <gurbuz.duygu@gmail.com>
2021-05-16 21:54:12 +0200

    http links are fixed.

--- script.js
+++ script.js
@@ -121,7 +121,7 @@ function displayForecast(response) {
 
 function getForecast (coordinates){
     let apiKey = "ed0b705eed2cc81cd6b87bb194b4d152";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+    let apiUrl = `http://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(displayForecast);
 
 }
@@ -180,7 +180,7 @@ function displayTemperature (response){
 
 function search (city){
     let apiKey = "ed0b705eed2cc81cd6b87bb194b4d152";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     
     
     axios.get(apiUrl).then(displayTemperature);
@@ -217,7 +217,7 @@ function showLocation (position){
 
     let apiKey = "ed0b705eed2cc81cd6b87bb194b4d152";
 
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&&appid=${apiKey}&units=metric`;
+    let apiUrl = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&&appid=${apiKey}&units=metric`;
 
     axios.get(apiUrl).then(displayTemperature);
 }
